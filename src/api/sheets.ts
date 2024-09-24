9ac06ae69ef3f2a32a4b595c1ca16916c5bad3c5
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2024-09-24 12:42:42 +0300

    add logs

--- src/api/sheets.ts
+++ src/api/sheets.ts
@@ -2,15 +2,22 @@ import {IUser} from "../types/userTypes.ts";
 
 export const checkIfAlreadyCheckedIn = async (employeeId: IUser['id']) => {
     try {
+        console.log('idUser: ', employeeId)
 
         const response = await fetch(`${import.meta.env.VITE_GOOGLESHEETS_URL}?employeeId=${employeeId}`, {
             method: 'GET',
             mode: 'no-cors',
         });
 
-        return await response.json();
+        console.log('response: ', response);
 
-    } catch {
+        const responseJson = await response.json();
+        console.log('responseJson: ', responseJson);
+
+        return responseJson;
+
+    } catch (error) {
+        console.log('error: ', error);
         return false;
     }
 };
